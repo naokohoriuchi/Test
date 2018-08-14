cc02b893dbf5f9c243cc9a9f6c06549fcb9a142e
堀内奈緒子 <gossprinter33@gmail.com>
2018-08-14 23:25:00 +0900

    20180814 SQL

---
+++ sastruts/src/main/resources/org/seasar/sastruts/example/service/setTransportationData.sql
@@ -0,0 +1,28 @@
+INSERT INTO TRANSPORTATION (
+	WRITE_DATE,
+	DEPARTMENT,
+	NAME,
+	BOARDING_DATE,
+	DESTINATION,
+	TYPE
+	DEPARTURE,
+	ALLOW,
+	DESTINATION_STATION,
+	MONEY,
+	REMARKS,
+	TOTAL
+)
+VALUES (
+	/*writeDate*/,
+	/*department*/,
+	/*name*/,
+	/*boardingDateList*/,
+	/*destinationList*/,
+	/*typeList*/,
+	/*departureList*/,
+	/*arrowList*/,
+	/*destinationStationList*/,
+	/*moneyList*/,
+	/*remarksList*/,
+	/*total*/
+);
