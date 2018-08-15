e589fe403c397b3fb2e650440ad68687dd388e65
堀内奈緒子 <gossprinter33@gmail.com>
2018-08-16 00:46:09 +0900

    @Component削除

--- sastruts/src/main/java/org/seasar/sastruts/example/dto/PrmTransportationDto.java
+++ sastruts/src/main/java/org/seasar/sastruts/example/dto/PrmTransportationDto.java
@@ -2,15 +2,11 @@ package org.seasar.sastruts.example.dto;
 
 import java.io.Serializable;
 
-import org.seasar.framework.container.annotation.tiger.Component;
-import org.seasar.framework.container.annotation.tiger.InstanceType;
-
 /**
  * パラメータDTO
  *
  * @author Naoko Horiuchi
  */
-@Component(instance = InstanceType.SESSION)
 public class PrmTransportationDto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
