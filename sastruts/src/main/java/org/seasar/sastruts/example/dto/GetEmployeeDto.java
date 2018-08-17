1ca18329c8da23142b5de73d9dae6b357a3f94e4
堀内奈緒子 <gossprinter33@gmail.com>
2018-08-17 18:41:58 +0900

    社員取得結果格納dto

---
+++ sastruts/src/main/java/org/seasar/sastruts/example/dto/GetEmployeeDto.java
@@ -0,0 +1,16 @@
+package org.seasar.sastruts.example.dto;
+
+/**
+ * 社員一覧格納用DTO
+ *
+ * @author Naoko Horiuchi
+ */
+public class GetEmployeeDto {
+
+	/** 部署名 */
+	public String department;
+
+	/** 名前 */
+	public String name;
+
+}
